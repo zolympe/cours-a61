ba3e4931ec5158fa7fe742b18a2c19dff1f5c831
Zolympe <fabrice.mulotti@gmail.com>
2021-02-14 06:24:23 -0500

    Partie 13 : Circle CI

--- packages/regression_model/setup.py
+++ packages/regression_model/setup.py
@@ -10,14 +10,10 @@ from setuptools import find_packages, setup
 
 # Package meta-data.
 NAME = 'regression_model'
-# DESCRIPTION = 'Train and deploy regression model.'
-# URL = 'your github project'
-# EMAIL = 'your_email@email.com'
-# AUTHOR = 'Your name'
-DESCRIPTION = 'Former et deployer un modele de regression.'
-URL = 'https://github.com/cegep2020/cours-A61.git'
-EMAIL = 'aamami@csfoy.ca'
-AUTHOR = 'Cegep AA'
+DESCRIPTION = 'Train and deploy regression model.'
+URL = 'your github project'
+EMAIL = 'your_email@email.com'
+AUTHOR = 'Your name'
 REQUIRES_PYTHON = '>=3.6.0'
 
 
